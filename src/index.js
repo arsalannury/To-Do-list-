31d5a4ea52c40551e0638fc335ae44ff80ba7ff0
arsalannury <arsiiiland@yaho.com>
2021-12-14 18:19:12 +0330

    start biuld dark mode

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-// import Toastify from 'toastify-js'
-// import Darkmode from 'darkmode-js';
-import  persianDate  from 'persian-date';
-// const options = {
-//     bottom: '30px', 
-//     right: '30px', 
-//     left: 'unset', 
-//     time: '1s',
-//     mixColor: '#fff', 
-//     backgroundColor: '#fff',  
-//     buttonColorDark: '#100f2c', 
-//     buttonColorLight: '#fff', 
-//     saveInCookies: false, 
-//     label: '🌓',
-//     autoMatchOsTheme: true ,
-//     cursor: 'pointer',
-//   }
-//   const darkmode =  new Darkmode(options);
-// darkmode.showWidget()
-// const date = new persianDate().format()
-// console.log(date)
-const datePersian = document.querySelector('.date_persian');
+import Toastify from "toastify-js";
+import persianDate from "persian-date";
 
-const updateTime = setInterval(()=> {
- var innerTexDateElement = new persianDate().format().toString();
- datePersian.innerText =  innerTexDateElement
-},1000)
+
+const darkModeIconMoon = document.querySelector('#darkmode_icon');
+const darkModeIconSun = document.querySelector('.darkmode_icon_sun');
+const hamburgerIcon = document.querySelector('.hamburger-inner');
+const modalContent = document.querySelector('.modal-content');
+
+
+// pesrian date show to navbar menu start |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+const datePersian = document.querySelector(".date_persian");
+const updateTime = setInterval(() => {
+  var innerTexDateElement = new persianDate().format().toString();
+  datePersian.innerText = innerTexDateElement;
+}, 1000);
+// pesrian date show to navbar menu end |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+// dark Mode web page start |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+darkModeIconMoon.addEventListener('click',(e) => {
+darkModeStyles();
+})
+function darkModeStyles(){
+    document.body.style.backgroundColor = '#111';
+    document.body.style.color = '#fff';
+    darkModeIconMoon.className = 'bi bi-brightness-high darkmode_icon_sun';
+    hamburgerIcon.style.backgroundColor = '#fff';
+    modalContent.style.backgroundColor = '#aaa';
+}
+// dark Mode web page end |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
