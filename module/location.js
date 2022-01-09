d16935bb40cc6953fb164f9d298df6b9b00067f1
arsalannury <arsiiiland@yaho.com>
2022-01-09 17:33:54 +0330

    index js end

--- module/location.js
+++ module/location.js
@@ -5,6 +5,12 @@ const changeLocation = () => {
       document.querySelector('.done_page').onclick = () => {
         location.href = './Done.html'
       }
+      document.querySelector('.delete_page').onclick = () => {
+        location.href = './Delete.html'
+      }
+      document.querySelector('.about_me').onclick = () => {
+        location.href = './aboutMe.html'
+      }
 }
 
 export default changeLocation
