dc603c6be987e6cb7c9f2cd0b13732bfb1b49b10
arsalannury <arsiiiland@yaho.com>
2022-01-14 23:45:36 +0330

    add dark mode component

--- module/rootJs.js
+++ module/rootJs.js
@@ -1,40 +1,18 @@
 import Toastify from "toastify-js";
-import persianDate from "persian-date";
-import changeLocation from "../module/location";
-
-const darkModeIconMoon = document.querySelector("#darkmode_icon");
-const aquariumBody = document.querySelector(".aquarium");
+const modalContent = document.querySelector(".modal-content");
+const modalBackdrop = document.getElementById("exampleModal");
 const aquariumTable = document.querySelector(".aquarium__table");
+const aquariumBody = document.querySelector(".aquarium");
 const aquariumAquarium = document.querySelector(".aquarium__aquarium");
 const navbarContainer = document.querySelector(".menu_container");
+const hamburgerNavbarBtn = document.querySelector(".hamburger_navbar");
+const navbarBackdrop = document.querySelector(".backdrop");
+const darkModeIconMoon = document.querySelector("#darkmode_icon");
 const navbarSvg = document.querySelectorAll(".navbar_svg path");
 const navbarListItem = document.querySelectorAll(".menu_container ul li");
-const navbarBackdrop = document.querySelector(".backdrop");
-const hamburgerNavbarBtn = document.querySelector(".hamburger_navbar");
-const getLocalStorageToJson = JSON.parse(localStorage.getItem("DeleteLocal"));
-const cardContainer = document.querySelector(".card_container");
-const paragraphEmptyList = document.querySelector(".nothing_to_do");
-
-changeLocation();
-// pesrian date show to navbar menu start |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-const datePersian = document.querySelector(".date_persian");
-const updateTime = setInterval(() => {
-  var innerTexDateElement = new persianDate().format().toString();
-  datePersian.innerText = innerTexDateElement;
-}, 1000);
-// pesrian date show to navbar menu end |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-
 // light and dark Mode web page start |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-darkModeIconMoon.addEventListener("click", (e) => {
-  if (document.body.id === "true") {
-    darkModeStyles();
-  } else if (document.body.id === "false") {
-    lightModeStyles();
-  }
-});
-
-function darkModeStyles() {
+export function darkModeStyles() {
   Toastify({
     text: "Dark mode",
     position: "center",
@@ -48,6 +26,9 @@ function darkModeStyles() {
   document.body.style.backgroundColor = "#111";
   document.body.style.color = "#fff";
   darkModeIconMoon.className = "bi bi-brightness-high darkmode_icon_sun";
+  if(modalContent){
+    modalContent.style.backgroundColor = "#aaa";
+  }
   aquariumBody.style.backgroundColor = "#111";
   aquariumTable.style.backgroundColor = "#fff";
   aquariumAquarium.style.borderColor = "#fff";
@@ -70,6 +51,9 @@ function darkModeStyles() {
   });
   navbarContainer.style.boxShadow = "0 0 10px #eee";
   navbarContainer.style.backgroundColor = "#111";
+  if(modalContent){
+    modalBackdrop.style.backgroundColor = "rgba(255,255,255,.20)";
+  }
   navbarBackdrop.style.backgroundColor = "rgba(255,255,255,.20)";
   navbarListItem.forEach((item) => {
     item.style.color = "rgba(255,255,255,.5)";
@@ -77,7 +61,7 @@ function darkModeStyles() {
   document.body.id = "false";
 }
 
-function lightModeStyles() {
+export function lightModeStyles() {
   Toastify({
     text: "Light mode",
     position: "center",
@@ -86,6 +70,9 @@ function lightModeStyles() {
   document.body.style.backgroundColor = "#fff";
   document.body.style.color = "unset";
   darkModeIconMoon.className = "bi bi-moon darkmode_icon";
+  if(modalContent){
+    modalContent.style.backgroundColor = "#fff";
+  }
   aquariumBody.style.backgroundColor = "#fff";
   aquariumTable.style.backgroundColor = "#000";
   aquariumAquarium.style.borderColor = "#000";
@@ -111,6 +98,9 @@ function lightModeStyles() {
   });
   navbarContainer.style.boxShadow = "0 0 10px #333";
   navbarContainer.style.backgroundColor = "#fff";
+  if(modalContent){
+      modalBackdrop.style.backgroundColor = "unset";
+  }
   navbarBackdrop.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
   navbarListItem.forEach((item) => {
     item.style.color = "#0000";
@@ -121,26 +111,15 @@ function lightModeStyles() {
 
 // show and hide navbar start |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-hamburgerNavbarBtn.addEventListener("click", (e) => {
-  if (hamburgerNavbarBtn.id === "true") {
-    navbarBtnHandlerShow();
-  } else if (hamburgerNavbarBtn.id === "false") {
-    navbarBtnHandlerHide();
-  }
-});
 
-navbarBackdrop.addEventListener("click", (e) => {
-  navbarBtnHandlerHide();
-});
-
-function navbarBtnHandlerShow() {
+export function navbarBtnHandlerShow() {
   hamburgerNavbarBtn.classList.add("is-active");
   navbarContainer.style.transform = "translateX(0)";
   navbarBackdrop.style.display = "unset";
   hamburgerNavbarBtn.id = "false";
 }
 
-function navbarBtnHandlerHide() {
+export function navbarBtnHandlerHide() {
   hamburgerNavbarBtn.classList.remove("is-active");
   navbarContainer.style.transform = "translateX(-300px)";
   navbarBackdrop.style.display = "none";
@@ -148,42 +127,39 @@ function navbarBtnHandlerHide() {
 }
 // show and hide navbar end |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-
-
-// after load page
-// show card when we have done local storage
-const showCardAfterLoad = () => {
-  window.addEventListener("load", (e) => {
-    if (getLocalStorageToJson) {
-      cardContainer.style.flexDirection = "row";
-      aquariumBody.style.display = "none";
-      paragraphEmptyList.style.display = "none";
-
-      if (getLocalStorageToJson.length >= 6) {
-        const newDeleteLocal = getLocalStorageToJson.shift();
-        localStorage.setItem(
-          "DeleteLocal",
-          JSON.stringify(getLocalStorageToJson)
+// when add card ,if darkmode was on
+// card has darkmode styles
+// and if light mode was on
+// card has light mode styles
+export function styleCardDarkOrLightMode() {
+    if (document.body.id === "true") {
+      document
+        .querySelectorAll(".card-subtitle")
+        .forEach((subtitle) => (subtitle.style.color = "#000"));
+      document
+        .querySelectorAll(".card-text")
+        .forEach((cardText) => (cardText.style.color = "#000"));
+      document
+        .querySelectorAll(".card")
+        .forEach((bgColor) => (bgColor.style.backgroundColor = "#fff"));
+      document
+        .querySelectorAll(".card")
+        .forEach(
+          (colorBorder) =>
+            (colorBorder.style.border = "1px solid rgba(0,0,0,.125)")
         );
-      }
-
-      getLocalStorageToJson.forEach((item, index) => {
-        cardContainer.innerHTML += `
-         <div id='${item.id}' class="card card_anime">
-         <div class="card-body">
-         <p class="card-subtitle">${item.time}</p>
-         <p class="card-text">
-           ${item.text}
-         </p>
-         </div>
-         <div class="card_footer">
-           <img class="card_image_footer" src="../images/icons8-close.svg" />
-         </div>
-         </div> 
-
-         `;
-      });
+    } else if (document.body.id === "false") {
+      document
+        .querySelectorAll(".card-subtitle")
+        .forEach((subtitle) => (subtitle.style.color = "#fff"));
+      document
+        .querySelectorAll(".card-text")
+        .forEach((cardText) => (cardText.style.color = "#fff"));
+      document
+        .querySelectorAll(".card")
+        .forEach((bgColor) => (bgColor.style.backgroundColor = "#333"));
+      document
+        .querySelectorAll(".card")
+        .forEach((borderColor) => (borderColor.style.border = "1px solid #fff"));
     }
-  });
-};
-showCardAfterLoad();
+  }
--- src/Done.js
+++ src/Done.js
@@ -1,7 +1,12 @@
 import Toastify from "toastify-js";
 import persianDate from "persian-date";
 import changeLocation from "../module/location";
-
+import {
+  darkModeStyles,
+  lightModeStyles,
+  navbarBtnHandlerShow,
+  navbarBtnHandlerHide,
+}from '../module/rootJs';
 const darkModeIconMoon = document.querySelector("#darkmode_icon");
 const aquariumBody = document.querySelector(".aquarium");
 const aquariumTable = document.querySelector(".aquarium__table");
@@ -16,13 +21,7 @@ const cardContainer = document.querySelector(".card_container");
 const paragraphEmptyList = document.querySelector(".nothing_to_do");
 
 changeLocation();
-// pesrian date show to navbar menu start |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-const datePersian = document.querySelector(".date_persian");
-const updateTime = setInterval(() => {
-  var innerTexDateElement = new persianDate().format().toString();
-  datePersian.innerText = innerTexDateElement;
-}, 1000);
-// pesrian date show to navbar menu end |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
 
 // light and dark Mode web page start |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
@@ -34,89 +33,7 @@ darkModeIconMoon.addEventListener("click", (e) => {
   }
 });
 
-function darkModeStyles() {
-  Toastify({
-    text: "Dark mode",
-    position: "center",
-    duration: 3000,
-    style: {
-      backgroundImage: "unset",
-      backgroundColor: "#fff",
-      color: "#111",
-    },
-  }).showToast();
-  document.body.style.backgroundColor = "#111";
-  document.body.style.color = "#fff";
-  darkModeIconMoon.className = "bi bi-brightness-high darkmode_icon_sun";
-  aquariumBody.style.backgroundColor = "#111";
-  aquariumTable.style.backgroundColor = "#fff";
-  aquariumAquarium.style.borderColor = "#fff";
-  if (document.querySelector(".card")) {
-    document
-      .querySelectorAll(".card-subtitle")
-      .forEach((subtitle) => (subtitle.style.color = "#fff"));
-    document
-      .querySelectorAll(".card-text")
-      .forEach((cardText) => (cardText.style.color = "#fff"));
-    document
-      .querySelectorAll(".card")
-      .forEach((bgColor) => (bgColor.style.backgroundColor = "#333"));
-    document
-      .querySelectorAll(".card")
-      .forEach((borderColor) => (borderColor.style.border = "1px solid #fff"));
-  }
-  navbarSvg.forEach((svg) => {
-    svg.style.fill = "#fff";
-  });
-  navbarContainer.style.boxShadow = "0 0 10px #eee";
-  navbarContainer.style.backgroundColor = "#111";
-  navbarBackdrop.style.backgroundColor = "rgba(255,255,255,.20)";
-  navbarListItem.forEach((item) => {
-    item.style.color = "rgba(255,255,255,.5)";
-  });
-  document.body.id = "false";
-}
 
-function lightModeStyles() {
-  Toastify({
-    text: "Light mode",
-    position: "center",
-    duration: 3000,
-  }).showToast();
-  document.body.style.backgroundColor = "#fff";
-  document.body.style.color = "unset";
-  darkModeIconMoon.className = "bi bi-moon darkmode_icon";
-  aquariumBody.style.backgroundColor = "#fff";
-  aquariumTable.style.backgroundColor = "#000";
-  aquariumAquarium.style.borderColor = "#000";
-  if (document.querySelector(".card")) {
-    document
-      .querySelectorAll(".card-subtitle")
-      .forEach((subtitle) => (subtitle.style.color = "#000"));
-    document
-      .querySelectorAll(".card-text")
-      .forEach((cardText) => (cardText.style.color = "#000"));
-    document
-      .querySelectorAll(".card")
-      .forEach((bgColor) => (bgColor.style.backgroundColor = "#fff"));
-    document
-      .querySelectorAll(".card")
-      .forEach(
-        (colorBorder) =>
-          (colorBorder.style.border = "1px solid rgba(0,0,0,.125)")
-      );
-  }
-  navbarSvg.forEach((svg) => {
-    svg.style.fill = "#0099ff";
-  });
-  navbarContainer.style.boxShadow = "0 0 10px #333";
-  navbarContainer.style.backgroundColor = "#fff";
-  navbarBackdrop.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  navbarListItem.forEach((item) => {
-    item.style.color = "#0000";
-  });
-  document.body.id = "true";
-}
 // light Mode and dark mode web page end |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 // show and hide navbar start |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -133,19 +50,7 @@ navbarBackdrop.addEventListener("click", (e) => {
   navbarBtnHandlerHide();
 });
 
-function navbarBtnHandlerShow() {
-  hamburgerNavbarBtn.classList.add("is-active");
-  navbarContainer.style.transform = "translateX(0)";
-  navbarBackdrop.style.display = "unset";
-  hamburgerNavbarBtn.id = "false";
-}
 
-function navbarBtnHandlerHide() {
-  hamburgerNavbarBtn.classList.remove("is-active");
-  navbarContainer.style.transform = "translateX(-300px)";
-  navbarBackdrop.style.display = "none";
-  hamburgerNavbarBtn.id = "true";
-}
 // show and hide navbar end |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
