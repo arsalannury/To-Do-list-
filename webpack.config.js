32497a85f27ec003b822aadc83eccd6017775b17
arsalannury <arsiiiland@yaho.com>
2022-01-16 16:03:20 +0330

    add about me page

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
     home: { import: './src/index.js', filename: './dist/main.js' },
     done: { import: './src/Done.js', filename: './dist/done.js' },
     delete : {import: './src/Delete.js', filename: './dist/delete.js'},
+    aboutMe: {import: './src/AboutMe.js', filename: './dist/aboutMe.js'}
   },
   
 };
