b582f53bc9259d3a25d56ad93c53ba39ba8fae0e
arsalannury <arsiiiland@yaho.com>
2022-01-19 16:02:33 +0330

    about me end

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,6 +2,7 @@ import Toastify from "toastify-js";
 import persianDate from "persian-date";
 import changeLocation from "../module/location";
 import fullpage from "fullpage.js";
+import screenfull from 'screenfull';
 
 import {
     darkModeStyles,
@@ -12,8 +13,9 @@ import {
   const darkModeIconMoon = document.querySelector("#darkmode_icon");
   const navbarBackdrop = document.querySelector(".backdrop");
   const hamburgerNavbarBtn = document.querySelector(".hamburger_navbar");
-
-
+  const myImage = document.querySelector('#me');
+  const containerAboutMe = document.querySelector('.container_about_me');
+  const arrows = document.querySelectorAll('.arrows i');
 
 
 changeLocation();
@@ -30,8 +32,10 @@ const updateTime = setInterval(() => {
 darkModeIconMoon.addEventListener("click", (e) => {
   if (document.body.id === "true") {
     darkModeStyles();
+    aboutMeDarkLightMode('#fff','#fff')
   } else if (document.body.id === "false") {
     lightModeStyles();
+    aboutMeDarkLightMode('#333','#000')
   }
 });
 // light and dark Mode web page end |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -51,6 +55,8 @@ navbarBackdrop.addEventListener("click", (e) => {
 });
 // show and hide navbar end |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
+
+// new scroll model page with fullpage start |||||||||||||||||||||||||||||||||||||||||||
 new fullpage('#fullpage', {
   licenseKey: '7yhihj7ht67hyh',
 	scrollHorizontally: true,
@@ -63,7 +69,27 @@ new fullpage('#fullpage', {
   keyboardScrolling: true,
   anchors:['start', 'end'],
 });
-
-//methods
 fullpage_api.setAllowScrolling(true);
+// new scroll model page with fullpage end |||||||||||||||||||||||||||||||||||||||||||
 
+
+
+
+
+
+
+// click image and view full screen image start ||||||||||||||||||||||||||||||||||||
+myImage.addEventListener('click', () => {
+	if (screenfull.isEnabled) {
+		screenfull.request(myImage);
+	}
+});
+// click image and view full screen image end ||||||||||||||||||||||||||||||||||||
+
+
+function aboutMeDarkLightMode(params) {
+  containerAboutMe.style.color = params;
+  arrows.forEach((arrow) => {
+    arrow.style.color = '#fff';
+  })
+}
